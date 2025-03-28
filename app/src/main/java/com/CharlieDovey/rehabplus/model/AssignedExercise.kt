309ccfe8716abb2b39eabd3b922e5c2f6133a8ca
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2025-03-28 00:31:39 +0000

    Added AssignedExercise class for user specific exercise tracking using composition with Exercise

---
+++ app/src/main/java/com/CharlieDovey/rehabplus/model/AssignedExercise.kt
@@ -0,0 +1,14 @@
+package com.charliedovey.rehabplus.model
+
+/**
+ * AssignedExercise is a custom version of a base Exercise,
+ * allowing a physiotherapist to specify user-specific reps, sets, and hold time.
+ * This uses composition, referencing the Exercise by ID.
+ */
+data class AssignedExercise(
+    val exerciseId: String,
+    var reps: Int,
+    var sets: Int,
+    var holdTime: Int = 0,
+    var isComplete: Boolean = false
+)
